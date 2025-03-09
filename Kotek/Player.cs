c8d28e85b7672903ef0e92280715ea227ba36566
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-03-09 19:55:56 +0100

    First meeting

---
+++ Kotek/Player.cs
@@ -0,0 +1,30 @@
+class Player
+{
+    public string name;
+    public int x;
+    public int y;
+
+    public Player(string name)
+    {
+        this.name = name;
+    }
+}
+
+/*
+Klasa Player:
+ - dane:
+    - hp
+    - maxHp
+    - attackStrength
+    - stamina
+    - maxStamina
+    - name
+    - position
+    - inventory
+ - akcje:
+    - attack
+    - heal
+    - move
+    - pickUpItem
+    - useItem
+*/
---
+++ Kotek/Program.cs
@@ -0,0 +1,35 @@
+﻿Player hero = new Player("Snake");
+
+Console.WriteLine($"({hero.x}, {hero.y})");
+
+while (true)
+{
+    ConsoleKeyInfo pressedKeyInfo = Console.ReadKey(true);
+    if (pressedKeyInfo.Key == ConsoleKey.A)
+    {
+        hero.x -= 1;
+    }
+    if (pressedKeyInfo.Key == ConsoleKey.D)
+    {
+        hero.x += 1;
+    }
+    if (pressedKeyInfo.Key == ConsoleKey.W)
+    {
+        hero.y += 1;
+    }
+    if (pressedKeyInfo.Key == ConsoleKey.S)
+    {
+        hero.y -= 1;
+    }
+    Console.SetCursorPosition(0, 0);
+    Console.WriteLine($"({hero.x}, {hero.y})");
+}
+
+Console.WriteLine("Press Space to continue...");
+ConsoleKeyInfo keyInfo;
+
+do
+{
+    keyInfo = Console.ReadKey(true);
+} while (keyInfo.Key != ConsoleKey.Spacebar);
+
