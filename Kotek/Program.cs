9d0aabcc54bd490b6456f796ebd90cbe63fb9eac
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-04-13 12:37:53 +0200

    Fix movement to use current speed

--- Kotek/Program.cs
+++ Kotek/Program.cs
@@ -1,13 +1,14 @@
-﻿Player hero = new Player("Snake");
-hero.speed = 3;
+﻿Dictionary<ConsoleKey, Point> directionsMap = new Dictionary<ConsoleKey, Point>();
+directionsMap.Add(ConsoleKey.A, new Point(-1, 0));
+directionsMap.Add(ConsoleKey.D, new Point(1, 0));
+directionsMap.Add(ConsoleKey.W, new Point(0, -1));
+directionsMap.Add(ConsoleKey.S, new Point(0, 1));
+
+Player hero = new Player("Snake");
+hero.speed = 1;
 hero.x = 1;
 hero.y = 2;
 
-Dictionary<ConsoleKey, Point> directionsMap = new Dictionary<ConsoleKey, Point>();
-directionsMap.Add(ConsoleKey.A, new Point(-hero.speed, 0));
-directionsMap.Add(ConsoleKey.D, new Point(hero.speed, 0));
-directionsMap.Add(ConsoleKey.W, new Point(0, -hero.speed));
-directionsMap.Add(ConsoleKey.S, new Point(0, hero.speed));
 
 while (true)
 {
@@ -29,11 +30,13 @@ while (true)
 
     Point direction = directionsMap[pressedKeyInfo.Key];
     
-    hero.x += direction.x;
-    hero.y += direction.y;
+    hero.x += direction.x * hero.speed;
+    hero.y += direction.y * hero.speed;
 
     hero.x = Math.Clamp(hero.x, 0, Console.BufferWidth - 1);
     hero.y = Math.Clamp(hero.y, 0, Console.BufferHeight - 1);
+
+    hero.speed += 1;
 }
 
 Console.WriteLine("Press Space to continue...");
