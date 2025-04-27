3553de346aa00ef5681b626a62a8ad7391552621
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-04-27 11:39:34 +0200

    Fix crash when redrawing cell outside of level

--- Kotek/Program.cs
+++ Kotek/Program.cs
@@ -69,8 +69,8 @@ while (true)
         element.position.x += direction.x * element.speed;
         element.position.y += direction.y * element.speed;
 
-        element.position.x = Math.Clamp(element.position.x, 0, Console.BufferWidth - 1);
-        element.position.y = Math.Clamp(element.position.y, 0, Console.BufferHeight - 1);
+        element.position.x = Math.Clamp(element.position.x, 0, level[0].Length - 1);
+        element.position.y = Math.Clamp(element.position.y, 0, level.Length - 1);
 
         // element.speed += 1;
     }
