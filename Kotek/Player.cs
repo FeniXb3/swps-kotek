d27f52e86d15722687d90ead3ba4b01e17786da0
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-06-01 11:19:57 +0200

    Extract moving to the Move method in Player class

--- Kotek/Player.cs
+++ Kotek/Player.cs
@@ -10,6 +10,48 @@ class Player
         this.name = name;
         this.avatar = avatar;
     }
+
+    public void Move(Point direction, string[] level)
+    {
+        Point target = position;
+
+        // target.x += direction.x * element.speed;
+        // target.y += direction.y * element.speed;
+
+        int signX = Math.Sign(direction.x);
+        for (int x = 1; x <= Math.Abs(direction.x * speed); x++)
+        {
+            int coordinateToTest = position.x + x * signX;
+            if (level[target.y][coordinateToTest] == '#')
+            {
+                break;
+            }
+
+            target.x = coordinateToTest;
+        }
+
+        int signY = Math.Sign(direction.y);
+        for (int y = 1; y <= Math.Abs(direction.y * speed); y++)
+        {
+            int coordinateToTest = position.y + y * signY;
+            if (level[coordinateToTest][target.x] == '#')
+            {
+                break;
+            }
+
+            target.y = coordinateToTest;
+        }
+
+        // HACK: We have to limit y before limiting x because we need row's lenght to limit x
+        target.y = Math.Clamp(target.y, 0, level.Length - 1);
+        target.x = Math.Clamp(target.x, 0, level[target.y].Length - 1);
+
+        if (level[target.y][target.x] != '#')
+        {
+            position = target;
+            speed += 1;
+        }
+    }
 }
 
 /*
--- Kotek/Program.cs
+++ Kotek/Program.cs
@@ -65,44 +65,7 @@ while (true)
         Console.Write(cellValue);
 
         Point direction = directionsMap[pressedKeyInfo.Key];
-        Point target = element.position;
-
-        // target.x += direction.x * element.speed;
-        // target.y += direction.y * element.speed;
-
-        int signX = Math.Sign(direction.x);
-        for (int x = 1; x <= Math.Abs(direction.x * element.speed); x++)
-        {
-            int coordinateToTest = element.position.x + x * signX;
-            if (level[target.y][coordinateToTest] == '#')
-            {
-                break;
-            }
-
-            target.x = coordinateToTest;
-        }
-
-        int signY = Math.Sign(direction.y);
-        for (int y = 1; y <= Math.Abs(direction.y * element.speed); y++)
-        {
-            int coordinateToTest = element.position.y + y * signY;
-            if (level[coordinateToTest][target.x] == '#')
-            {
-                break;
-            }
-
-            target.y = coordinateToTest;
-        }
-
-        // HACK: We have to limit y before limiting x because we need row's lenght to limit x
-        target.y = Math.Clamp(target.y, 0, level.Length - 1);
-        target.x = Math.Clamp(target.x, 0, level[target.y].Length - 1);
-
-        if (level[target.y][target.x] != '#')
-        {
-            element.position = target;
-            element.speed += 1;
-        }
+        element.Move(direction, level);
     }
 }
 
