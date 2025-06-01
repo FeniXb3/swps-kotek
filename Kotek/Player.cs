64f9ae0129bddddd2b5f932e272b8b618204e583
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-06-01 12:39:17 +0200

    Make Player inherit from Character

--- Kotek/Player.cs
+++ Kotek/Player.cs
@@ -1,9 +1,5 @@
-class Player
+class Player : Character
 {
-    public string name;
-    public Point position;
-    public int speed = 1;
-    public string avatar;
     Dictionary<ConsoleKey, string> keyActionsMap = new Dictionary<ConsoleKey, string>
     {
         {ConsoleKey.A, "moveLeft"},
@@ -13,61 +9,11 @@ class Player
         {ConsoleKey.C, "clone"},
     };
 
-    public Player(string name, string avatar)
+    public Player(string name, string avatar) : base(name, avatar)
     {
-        this.name = name;
-        this.avatar = avatar;
     }
 
-    public void Move(Point direction, string[] level)
-    {
-        Point target = position;
-
-        // target.x += direction.x * element.speed;
-        // target.y += direction.y * element.speed;
-
-        int signX = Math.Sign(direction.x);
-        for (int x = 1; x <= Math.Abs(direction.x * speed); x++)
-        {
-            int coordinateToTest = position.x + x * signX;
-            if (level[target.y][coordinateToTest] == '#')
-            {
-                break;
-            }
-
-            target.x = coordinateToTest;
-        }
-
-        int signY = Math.Sign(direction.y);
-        for (int y = 1; y <= Math.Abs(direction.y * speed); y++)
-        {
-            int coordinateToTest = position.y + y * signY;
-            if (level[coordinateToTest][target.x] == '#')
-            {
-                break;
-            }
-
-            target.y = coordinateToTest;
-        }
-
-        // HACK: We have to limit y before limiting x because we need row's lenght to limit x
-        target.y = Math.Clamp(target.y, 0, level.Length - 1);
-        target.x = Math.Clamp(target.x, 0, level[target.y].Length - 1);
-
-        if (level[target.y][target.x] != '#')
-        {
-            position = target;
-            speed += 1;
-        }
-    }
-
-    public void Display()
-    {
-        Console.SetCursorPosition(position.x, position.y);
-        Console.Write(avatar);
-    }
-
-    public string ChooseAction()
+    public override string ChooseAction()
     {
         ConsoleKeyInfo pressedKeyInfo = Console.ReadKey(true);
         string chosenAction = keyActionsMap.GetValueOrDefault(pressedKeyInfo.Key, "pass");
@@ -75,22 +21,3 @@ class Player
         return chosenAction;
     }
 }
-
-/*
-Klasa Player:
- - dane:
-    - hp
-    - maxHp
-    - attackStrength
-    - stamina
-    - maxStamina
-    - name
-    - position
-    - inventory
- - akcje:
-    - attack
-    - heal
-    - move
-    - pickUpItem
-    - useItem
-*/
