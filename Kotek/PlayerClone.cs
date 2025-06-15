147ec516feddbb2311918d8f46b8bb6b2cf8c4aa
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-06-15 11:33:45 +0200

    Limit allowed actions for clones

--- Kotek/PlayerClone.cs
+++ Kotek/PlayerClone.cs
@@ -1,14 +1,27 @@
 class PlayerClone : Player
 {
     Player whosClone;
+    List<string> allowedActions;
 
     public PlayerClone(Player whosClone, string avatar) : base(whosClone.name, avatar)
     {
         this.whosClone = whosClone;
+        allowedActions = new List<string>
+        {
+            "moveLeft",
+            "moveRight",
+            "moveUp",
+            "moveDown",
+        };
     }
 
     public override string ChooseAction()
     {
+        if (!allowedActions.Contains(whosClone.chosenAction))
+        {
+            return "pass";
+        }
+        
         return whosClone.chosenAction;
     }
 }
